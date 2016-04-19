ca4aea716284a5b83ea5838e2d6aa16859713f89
patkelly <patrickkelly261@gmail.com>
2016-04-19 01:53:06 +0100

    Initial Commit

---
+++ WorkChat/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WorkChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
